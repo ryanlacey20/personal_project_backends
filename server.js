6edb1dfced26dc772f7a13d1db339ae52a58efe5
Ryan Lacey <ryanpatricklacey@gmail.com>
2024-01-31 23:28:49 +0000

    recaptcha basic implementation

--- server.js
+++ server.js
@@ -1,35 +1,50 @@
-// server.js
+//server.js
 const express = require('express');
-const { post } = require('axios');
+const cors = require('cors');
 const { json } = require('body-parser');
-
+const qs = require('querystring');
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(json());
+app.use(cors());
 
 const secretKey = process.env.CAPTCHA_SECRET_KEY;
+const email = process.env.EMAIL;
+const phoneNumber = process.env.PHONENUMBER
+
 
 app.post('/api/verify-recaptcha', async (req, res) => {
     const recaptchaResponse = req.body.recaptchaResponse;
+    console.log("rekky", recaptchaResponse);
     try {
-        const verificationResult = await post('https://www.google.com/recaptcha/api/siteverify', {
-            secret: secretKey,
+        const params = new URLSearchParams({
+            secret: "6LcMi1UpAAAAANvYEz_l4pBKcxxmY-NLB4IQZGDt",
             response: recaptchaResponse,
         });
 
-        if (verificationResult.data.success) {
-            // Verified, serve the contact details
-            res.json({
-                success: true,
-                contactDetails: {
-                    email: 'your@email.com',
-                    linkedin: 'https://www.linkedin.com/in/yourusername/',
-                    github: 'https://github.com/yourusername',
-                },
+        fetch('https://www.google.com/recaptcha/api/siteverify', {
+            method: 'POST',
+            body: params,
+        })
+            .then(result => result.json())
+            .then(data => {
+                if (data.success) {
+                    // Verified, serve the contact details
+                    res.json({
+                        success: true,
+                        contactDetails: {
+                            phoneNumber: phoneNumber,
+                            email: email,
+                            linkedin: "https://www.linkedin.com/in/ryanapatricklacey/",
+                            github: "https://github.com/ryanlacey20",
+                        },
+                    });
+                } else {
+                    // Verification has failed
+                    res.json({ success: false });
+                }
             });
-        } else {
-            // Verification has failed
-            res.json({ success: false });
-        }
     } catch (error) {
         console.error('Error verifying reCAPTCHA:', error);
         res.status(500).json({ success: false });
